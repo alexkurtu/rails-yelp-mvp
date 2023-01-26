fba34780dbf271ac7619a684c7ebf1625997452e
Alex Kurtu <alex-kurtu@hotmail.com>
2023-01-26 20:31:57 +0100

    rails new with bootstrap & simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+ resources :restaurants, only: [:new, :show, :create, :index] do
+  resources :reviews, only: [:new, :create]
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian", phone_number: "108373543493"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian", phone_number: "0183344553"}
+pizza_hut = {name: "Pizza Hut", address: "56A High St, Berlin E1 6PQ", category: "italian", phone_number: "10837399493"}
+
+[dishoom, pizza_east, pizza_hut].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
